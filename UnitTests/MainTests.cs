6acab4fe1992f8df3a91bfc6dbe0458474cc3c95
pierrebaran <lothgar_pgm@hotmail.com>
2016-11-15 11:57:13 +0100

    Starting with Tests

---
+++ UnitTests/MainTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Drawing;
+using NSubstitute;
+using EdgeAplication;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MainTests
+    {
+        /*
+         * We'll first test our utilaries method used later in the tests to confirm they work correctly
+         */
+        [TestCategory("UtilitariesTest"), TestMethod()]
+        public void testBmpEqualTrue()
+        {
+            StreamReader streamReader = new StreamReader("..\\..\\Ressources\\Cobra.jpg");
+            //initialise original bitmap
+            Bitmap b1 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            Bitmap b2 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            Assert.IsTrue(Utilitaries.bmpEqual(b1, b2));
+        }
+
+        [TestCategory("UtilitariesTest"),TestMethod()]
+        public void testBmpEqualFalse()
+        {
+            StreamReader streamReader = new StreamReader("..\\..\\Ressources\\Cobra.jpg");
+            //initialise original bitmap
+            Bitmap b1 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader = new StreamReader("..\\..\\Ressources\\Rattlesnake.jpg");
+            Bitmap b2 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            Assert.IsFalse(Utilitaries.bmpEqual(b1, b2));
+        }
+
+        /*
+         * 
+         */
+        [TestCategory("ImageHandlerTest"), TestMethod()]
+        public void nominalImageLoading() {
+
+            var substituteHandler = Substitute.For<ImageHandlerInterface>();
+            Bitmap bmp = new Bitmap("..\\..\\Ressources\\Cobra.jpg");
+           
+
+            Assert.IsTrue(Utilitaries.bmpEqual(bmp, bmp));
+
+
+
+        } 
+
+    }
+}
---
+++ UnitTests/Utilitaries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+
+    public static class Utilitaries
+    {
+        
+        /*
+         * This method compares 2 bitmaps pixel by pixel and returns if they're equal
+         */ 
+        public static Boolean bmpEqual(Bitmap b1, Bitmap b2)
+        {
+            Boolean result = true;
+
+            if (b1.Width != b2.Width)
+                return false;
+            if (b1.Height != b2.Height)
+                return false;
+
+            for (int i = 0; i < b1.Width; i++)
+            {
+                for (int j = 0; j < b1.Height; j++)
+                {
+                    Color cb1 = b1.GetPixel(i, j);
+                    Color cb2 = b2.GetPixel(i, j);
+
+                    if ((cb1.A != cb2.A) || (cb1.R != cb2.R) || (cb1.G != cb2.G) || (cb1.B != cb2.B))
+                        return false;
+                }
+            }
+            return result;
+        }
+    }
+}
